d817913ef71b0c5b1d2ef24bd6aa62f4b2302407
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-10-14 09:19:49 +0200

    Updates

---
+++ precache-manifest.4fe462755ce0e71b291f774002f6211c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1e3cc2638ef79e182b3e828e17315a",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "f409c41bb3cc70d53645",
+    "url": "/restaurant-menu/static/css/main.24825034.chunk.css"
+  },
+  {
+    "revision": "64c642a630dd2b3f34c4",
+    "url": "/restaurant-menu/static/js/2.9d1c6742.chunk.js"
+  },
+  {
+    "revision": "f409c41bb3cc70d53645",
+    "url": "/restaurant-menu/static/js/main.e51a9221.chunk.js"
+  },
+  {
+    "revision": "336dd6ab319a881d9f75",
+    "url": "/restaurant-menu/static/js/runtime-main.1d80a21c.js"
+  },
+  {
+    "revision": "4efbf3ecd3179964f24c26228d35894e",
+    "url": "/restaurant-menu/static/media/1.4efbf3ec.jpg"
+  },
+  {
+    "revision": "cc09c01e15b3f256ae3dac11e61df6e1",
+    "url": "/restaurant-menu/static/media/10.cc09c01e.jpg"
+  },
+  {
+    "revision": "ec328b8e355c19427484b7d1488137df",
+    "url": "/restaurant-menu/static/media/11.ec328b8e.jpg"
+  },
+  {
+    "revision": "2b912421086478e73d6d6f2b3986094c",
+    "url": "/restaurant-menu/static/media/2.2b912421.jpg"
+  },
+  {
+    "revision": "931f3a4d6a4c36e353b3a331b8c02bd8",
+    "url": "/restaurant-menu/static/media/3.931f3a4d.jpg"
+  },
+  {
+    "revision": "4b2e6bc2b06e967c2a46f3c352f2092b",
+    "url": "/restaurant-menu/static/media/4.4b2e6bc2.jpg"
+  },
+  {
+    "revision": "3304bcdea685979be8d3ebfc27b92167",
+    "url": "/restaurant-menu/static/media/5.3304bcde.jpg"
+  },
+  {
+    "revision": "c7649d1839cfaf16b8977d152cd60324",
+    "url": "/restaurant-menu/static/media/6.c7649d18.jpg"
+  },
+  {
+    "revision": "9030c82858ec94c8e1013fa8af81c5c9",
+    "url": "/restaurant-menu/static/media/7.9030c828.jpg"
+  },
+  {
+    "revision": "58311d73e6605821d0cd314be1840cbe",
+    "url": "/restaurant-menu/static/media/8.58311d73.jpg"
+  },
+  {
+    "revision": "66a69658d869b456bbe574579423d72f",
+    "url": "/restaurant-menu/static/media/9.66a69658.jpg"
+  },
+  {
+    "revision": "7c1b6a5136525f7aa656b4c541920f87",
+    "url": "/restaurant-menu/static/media/inside.7c1b6a51.jpg"
+  },
+  {
+    "revision": "bac5b1fd2e8b2d392cc7a55ddb422500",
+    "url": "/restaurant-menu/static/media/tavern.bac5b1fd.jpg"
+  },
+  {
+    "revision": "eed0059821c341f66a63d2945f65d414",
+    "url": "/restaurant-menu/static/media/waiter.eed00598.jpg"
+  }
+]);
